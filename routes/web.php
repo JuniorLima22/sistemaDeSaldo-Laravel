8632d618fedac6b568a4cc6e529a7aeaf206191b
Francisco Lima <juniorlima.dev@gmail.com>
2021-10-01 15:46:10 -0300

    Criado formulário de recarga

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@
 Route::group(['middleware'=>'auth', 'namespace'=>'Admin', 'prefix'=>'admin'], function(){
     Route::get('/', ['as'=>'admin.home', 'uses'=>'AdminController@index']);
     Route::get('balance', ['as'=>'admin.balance', 'uses'=>'BalanceController@index']);
+    Route::get('deposit', ['as'=>'balance.deposit', 'uses'=>'BalanceController@deposit']);
+    Route::post('deposit', ['as'=>'deposit.store', 'uses'=>'BalanceController@depositStore']);
 });
 
 
